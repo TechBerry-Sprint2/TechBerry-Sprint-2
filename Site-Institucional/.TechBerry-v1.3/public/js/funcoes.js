13f950c9e1506c08364b6b609c9894aaba52421e
ryan-miyazato <ryan.miyazato@sptech.school>
2022-05-03 01:38:59 -0300

    Alterado CSS da Dash

--- Site-Institucional/.TechBerry-v1.3/public/js/funcoes.js
+++ Site-Institucional/.TechBerry-v1.3/public/js/funcoes.js
@@ -5,11 +5,14 @@ function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
+    var h1LoginUsuario = document.getElementById("h1_login_usuario");
 
     if (email != null && nome != null) {
         // window.alert(`Seja bem-vindo, ${nome}!`);
+        if (h1LoginUsuario != undefined) {
+            h1LoginUsuario.innerHTML = email;
+        }
         b_usuario.innerHTML = nome;
-        
 
         // finalizarAguardar();
     } else {
@@ -22,5 +25,33 @@ function limparSessao() {
     sessionStorage.clear();
     // finalizarAguardar();
     window.location = "../login.html";
+}
+
+// carregamento (loading)
+function aguardar() {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "flex";
+}
+
+function finalizarAguardar(texto) {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "none";
+
+    var divErrosLogin = document.getElementById("div_erros_login");
+    if (texto) {
+        divErrosLogin.innerHTML = texto;
+    }
+}
+
+
+// modal
+function mostrarModal() {
+    var divModal = document.getElementById("div_modal");
+    divModal.style.display = "flex";
+}
+
+function fecharModal() {
+    var divModal = document.getElementById("div_modal");
+    divModal.style.display = "none";
 }
 
